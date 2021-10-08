3ce618a12c454d05ef4f06e98a3f3678ddb882e1
pocachp <pocachip0318@gmail.com>
2021-10-09 00:33:13 +0900

    햄버거 버튼 작동 성공

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,6 +1,6 @@
-
-const menuTrigger = document.querySelector('.menu_trigger');
-menuTrigger.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('active');
-});
-
+window.onload=()=>{
+    const menuTrigger = document.querySelector('.menu_trigger');
+    menuTrigger.addEventListener('click', (event)=> {
+        event.currentTarget.classList.toggle('active')
+    });
+}
