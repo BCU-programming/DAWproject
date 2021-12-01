c9057fdd9054c1c797545236601593f97fe7a137
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-12-01 14:02:33 +0900

    .

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,7 +1,7 @@
 window.onload=()=>{
     const menuTrigger = document.querySelector('.menu_trigger');
     const menuWrap = document.querySelector(".menu_wrap");
-     menuTrigger.addEventListener('click', (event)=> {
+    menuTrigger.addEventListener('click', (event)=> {
         event.currentTarget.classList.toggle('active')
         if(menuWrap.classList.contains("on")) {
             menuWrap.classList.remove("on");
