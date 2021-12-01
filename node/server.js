afc670f1952c134f95430632d087704f1c075e9a
pocachp <pocachip0318@gmail.com>
2021-12-01 15:20:44 +0900

    폴더 경로 수정

--- node/server.js
+++ node/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3100);
 
 // server와 browser 연결될 때까지 기다리는 것
 app.listen(app.get('port'),() => {
@@ -11,5 +11,7 @@ app.listen(app.get('port'),() => {
 
 // 브라우저 뜨는 화면
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/main.html'));
+    res.sendFile(path.join(__dirname, '/main.html'));
 });
+
+app.use(express.static(__dirname + '/public'));
